5bcefba72599874f25ed5dac8097e9ea75d01cee
giodestr <destratisgiovanni2@gmail.com>
2025-03-20 16:40:12 +0100

    primo test

---
+++ Academy.ServiceLocatorPattern.BL/Entities/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy.ServiceLocatorPattern.BL.Entities
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Eta { get; set; }
+        
+
+    }
+}
---
+++ Academy.ServiceLocatorPattern.BL/Services/JsonDataService.cs
@@ -0,0 +1,39 @@
+﻿using Academy.ServiceLocatorPattern.BL.Entities;
+using Academy.ServiceLocatorPattern.BL.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy.ServiceLocatorPattern.BL.Services
+{    
+        public class JsonDataService : IDataService
+        {
+            private readonly string _filePath = @"C:\Users\destr\source\repos\Academy.ServiceLocatorPattern.Solution";
+            public Persona GetData(int id)
+            {
+                if (!File.Exists(_filePath)) return null;
+
+                var json = File.ReadAllText(_filePath);
+                var persons = JsonConvert.DeserializeObject<Persona[]>(json);
+                return persons.FirstOrDefault(p => p.Id == id);
+            }
+
+            public void SaveData(Persona persona)
+            {
+                var persons = File.Exists(_filePath)
+                 ? JsonConvert.DeserializeObject<Persona[]>(File.ReadAllText(_filePath))
+                 : Array.Empty<Persona>();
+
+                var list = persons.ToList();
+                list.Add(persona);
+
+                string json = JsonConvert.SerializeObject(list.ToArray(), Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(_filePath, json);
+            }
+        }
+    
+}
+
